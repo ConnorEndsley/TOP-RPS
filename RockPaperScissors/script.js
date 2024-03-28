3ac0e65e0a98669f9b27c96159a0a5dce7e4d9ba
Connor <endsleyconnor95@gmail.com>
2024-03-28 13:41:32 -0500

    complete computer guess

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -5,8 +5,16 @@
 // if any vs the same = tie and no point
 // first to 3 points wins
 
-
 // generate a random output of either rock, paper or scissors from the computers end
 // create an input for the human player
 // compare the users input to the computers input
 
+
+let attempt = ["rock", "paper", "scissors"];
+
+function playGame() {
+    let attemptValue = attempt.values();
+  let randomIdx= Math.floor(Math.random()*attempt.length);
+  let randomGuess = attempt[randomIdx];
+  console.log("computerGuess", randomGuess);
+};
